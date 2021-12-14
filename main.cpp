6bfdb99973b52f5ba9edb0cf505ac42b8abb7fc8
Hommyah <evil.hom26@gmail.com>
2021-12-14 12:42:01 +0300

    lr5

--- main.cpp
+++ main.cpp
@@ -1,40 +1,32 @@
 #include <iostream>
-#include <cmath>
-#include <iomanip>
 
 using namespace std;
 
 int main() {
-    double dx, x, y, a, eps, s, b;
-    int n = 1, N = 1;
-    eps = 0.001;
-    x = 0.2;
-    dx = fabs(0.2 - 0.8)/9;
 
-    cout << setw(2) << "n";
-    cout << setw(2) << "x";
-    cout << setw(8) << "s";
-    cout << setw(8) << "y" << endl;
-    while (N<=10) {
-        cout << setw(2) << N << "  ";
-        cout << setw(2) << setprecision(1) << x << "  ";
-        y = 0.25 * ((((x + 1) / sqrt(x)) * ((exp(sqrt(x)) - exp(-sqrt(x))) / 2)) - ((exp(sqrt(x)) + exp(-sqrt(x))) / 2));
-        n = 1;
-        a = x;
-        b = 1;
-        s = 0;
-        while (fabs(a) >= eps) {
-            b = b * x / (2*n*(2*n+1));
-            a = b * (pow(n,2));
-            s = s + a;
-            n++;
-        }
-        cout << setw(8) << setprecision(6) << s << "  ";
-        cout << setw(8) << setprecision(6) << y << endl ;
-        x+=dx;
-        N++;
+    int Matr[3][4];
+    int i, j;
+    int Mas[3] = {0};
 
+    cout << "Введите элементы матрицы" << endl;
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 4; j++)
+            cin >> Matr[i][j];
+
+    cout << "Матрица: " << endl;
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 4; j++)
+            cout << Matr[i][j] << " ";
+        cout << "\n";
     }
+    for (i=0; i<3; i++) {
+        for (j=0;j<4;j++)
+            Mas[i] += Matr[i][j];
+    }
+
+    cout << "Массив: ";
+    for (i=0; i<3; i++)
+    cout << Mas[i] << " ";
 
     return 0;
 }
