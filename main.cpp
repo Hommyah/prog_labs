70becaf868a63534cabff6e7fb197f46cfd80602
Hommyah <evil.hom26@gmail.com>
2021-12-14 13:20:37 +0300

    lr5+6

--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 
 using namespace std;
 
+//int Sum(int i, int j, int summ[i], int num[i][j]) {
+//    return (summ[i] += num[i][j]);
+//}
+
 int main() {
 
     int Matr[3][4];
     int i, j;
     int Mas[3] = {0};
+
 
     cout << "Введите элементы матрицы" << endl;
     for (i = 0; i < 3; i++)
@@ -19,6 +24,11 @@ int main() {
             cout << Matr[i][j] << " ";
         cout << "\n";
     }
+
+//    for (i=0; i<3; i++) {
+//        for (j=0;j<4;j++)
+//            Sum(i,j,Mas[i],Matr[i][j]);
+//    }
     for (i=0; i<3; i++) {
         for (j=0;j<4;j++)
             Mas[i] += Matr[i][j];
