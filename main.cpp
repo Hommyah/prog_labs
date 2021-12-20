a4bcb222696819ccbd795b1f1c022fffb3d07443
Hommyah <evil.hom26@gmail.com>
2021-12-20 20:42:54 +0300

    lr6

--- main.cpp
+++ main.cpp
@@ -2,42 +2,41 @@
 
 using namespace std;
 
-int Sum(int i,int j, int summ[i], int num[i][j]) {
+int Sum(const int matrix_str[], const int size) {
+    int sum = 0;
 
-            return (summ[i] += num[i][j]);
-
+    for (int i = 0; i < size; i++) {
+        sum += matrix_str[i];
+    }
+    return sum;
 }
 
 int main() {
-
-    int Matr[3][4];
+    const int str_cnt = 3, col_cnt = 4;
+    int Matr[str_cnt][col_cnt];
     int i, j;
-    int Mas[3] = {0};
-    int sum;
+    int Mas[str_cnt] = {};
 
     cout << "Введите элементы матрицы" << endl;
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 4; j++)
+    for (i = 0; i < str_cnt; i++)
+        for (j = 0; j < col_cnt; j++)
             cin >> Matr[i][j];
 
     cout << "Матрица: " << endl;
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 4; j++)
+    for (i = 0; i < str_cnt; i++) {
+        for (j = 0; j < col_cnt; j++)
             cout << Matr[i][j] << " ";
         cout << "\n";
     }
 
-        sum = Sum(i, j, Mas[i], Matr[i][j]);
-        //Mas[i] = sum;
 
-//    for (i=0; i<3; i++) {
-//        for (j=0;j<4;j++)
-//            Mas[i] += Matr[i][j];
-//    }
+    for (i = 0; i < str_cnt; i++) {
+        Mas[i] = Sum(Matr[i], col_cnt);
+    }
 
     cout << "Массив: ";
-    for (i=0; i<3; i++)
-    cout << Mas[i] << " ";
+    for (i = 0; i < str_cnt; i++)
+        cout << Mas[i] << " ";
 
     return 0;
 }
